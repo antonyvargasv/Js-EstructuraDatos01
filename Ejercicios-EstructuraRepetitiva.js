4f57abc628e9b76e84322ba0251fea87c8a6a602
Antony Vargas <73197177+antonyvargasv@users.noreply.github.com>
2020-11-17 00:13:07 -0500

    Agregando primer ejercicio, estructura repetititva

---
+++ Ejercicios-EstructuraRepetitiva.js
@@ -0,0 +1,53 @@
+/**
+ * ///////////////////////Ejercicio 01:
+ * Se requiere un algoritmo para obtener la suma de diez cantidades
+ * mediante la utilización de un ciclo “Mientras”.
+ */
+
+// console.log("*****EJERCICIO 01 ********** ");
+
+// let cantidad = 1;
+// let suma = 0;
+// let valor =0;
+
+// while (cantidad <= 10) {
+//   valor = +prompt("ingrese un numero");
+//   suma += valor;
+//   cantidad++;
+// }
+// console.log(`La suma de las cantidades ingresadas es : ${suma}`);
+
+/**
+ * ///////////////////////Ejercicio 02:
+ * Se requiere un algoritmo para obtener la suma de diez cantidades mediante la utilización de un ciclo Repite.
+ *
+ */
+
+// let cantidad = 1;
+// let suma = 0;
+// let valor = 0;
+// debugger;
+// do {
+//   valor = +prompt("ingrese un valor para sumar");
+//   suma += valor;
+//   cantidad++;
+// } while (cantidad <= 10);
+// {
+//   console.log(suma);
+// }
+
+/**
+ * ///////////////////////Ejercicio 03:
+ *
+ * Se requiere un algoritmo para obtener la suma de diez cantidades mediante la utilización de un ciclo Desde.
+ */
+
+
+let suma = 0;
+let valor = 0;
+
+for (let i = 0; i < 10; i++) {
+    valor = +prompt("ingrese un valor para la suma pe :V");
+    suma+= valor;
+}
+console.log(suma);
